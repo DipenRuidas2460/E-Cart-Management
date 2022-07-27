0a8d5c67e2af630371b569d18d803e053c26e3c1
Shilp1234 <106900895+Shilp1234@users.noreply.github.com>
2022-07-27 19:17:44 +0530

    Update validator.js

--- src/validators/validator.js
+++ src/validators/validator.js
@@ -1,12 +1,15 @@
 const isValid = function (x) {
-    let strRegex = /^[a-zA-Z,\-.\s]*$/
+    //let strRegex = /^[a-zA-Z,\-.\s]*$/
     if (typeof x === 'undefined' || x === null) return false
     if (typeof x != "string" ) return false
     if (typeof x === 'string' && x.trim().length === 0) return false
-    if (!strRegex.test(x)) return false
+    //if (!strRegex.test(x)) return false
     return true
 }
 
+const isValidBody = function (y) {
+    return Object.keys(y).length > 0
+}
 
 const isValidEmail = function (y) {
 
@@ -18,9 +21,38 @@ const isValidMobile = function (y) {
     let mobileRegex = /^(?:(?:\+|0{0,2})91(\s*[\-]\s*)?|[0]?)?[6789]\d{9}$/
     if (mobileRegex.test(y)) return true
 }
+// const isValidPrice=function(y){
+//     let priceRegex=/^(\d*([.,](?=\d{3}))?\d+)+((?!\2)[.,]\d\d)?$/
+//     if(priceRegex.test(y)) return true
+// }
 
 
-module.exports.isValid = isValid
-module.exports.isValidEmail = isValidEmail
-module.exports.isValidMobile = isValidMobile
+// const isValidString = (String) => {
+//     return /\d/.test(String)
+// }
+
+const isValidPrice = (price) => {
+    return /^[1-9]\d{0,7}(?:\.\d{1,2})?$/.test(price)
+}
+
+const  isValidSize=(size) =>{
+    let correctSize=["S", "XS", "M", "X", "L", "XXL", "XL"]
+    return (correctSize.includes(size))
+}
+
+const isValidEnum=function(value){
+    let availableSizes=["S", "XS", "M", "X", "L", "XXL", "XL"]
+    return availableSizes.includes(value)
+}
+
+const isValidNum=(number) => {
+    if(/^\d+$/.test(number)){
+        return true
+    }else{
+        return false
+    }
+}
+
+module.exports={isValidNum,isValidEnum,isValidBody,isValid,isValidEmail,isValidMobile,isValidSize,isValidPrice}
+
 
