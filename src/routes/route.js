d7edb4a635b9a124b0cb7861ddad242c7fae6bdb
Shilp1234 <106900895+Shilp1234@users.noreply.github.com>
2022-07-27 19:18:47 +0530

    Update route.js

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,16 +1,26 @@
 const  express  = require("express");
 const userController = require("../controllers/userController");
+const productController = require("../controllers/productController");
 const middleWare = require("../middleware/auth")
 const router = express.Router();
 
-
+/**********************************************[USER API]************************************************/
 router.post("/register",userController.createUser)
-
 router.post("/login",userController.loginUser)
-
 router.get("/user/:userId/profile",middleWare.authenticate,middleWare.authorize,userController.getProfile)
-
 router.put("/user/:userId/profile",middleWare.authenticate,middleWare.authorize,userController.updateUser)
+
+/**********************************************[PRODUCT API]************************************************/
+router.post("/products",productController.createProduct)
+
+router.get("/products",productController.getProduct)
+
+router.get("/products/:productId",productController.getProductByParams)
+
+router.put("/products/:productId",productController.updateProduct)
+
+router.delete("/products/:productId",productController.deleteProduct)
+
 
 
 module.exports = router
