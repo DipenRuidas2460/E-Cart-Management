ec8d4510ee41e767f7a3ab95f0574ea8768172fd
D2460 <dipenruidas1996@gmail.com>
2022-08-03 11:23:18 +0530

    in

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
 const app = express();
@@ -7,9 +7,8 @@ const app = express();
 const multer = require("multer");
 
 
-app.use(express.json());
+app.use(bodyParser.json());
 
-// app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(multer().any())
 
